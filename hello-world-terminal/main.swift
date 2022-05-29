4c24d4f66745325a89d371967893c41f01b07a1c
Nils <nilsgrimmer@gmx.de>
2022-05-29 13:57:37 +0200

    Test

--- hello-world-terminal/main.swift
+++ hello-world-terminal/main.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-print("Hello world...")
+func sayHello(name: String) {
+    print("Hello, \(name)...")
+}
 
+func printArgs(complete: Bool) {
+    let args = CommandLine.arguments
+    print("Es wurden \(args.count - 1) Parameter übergeben")
+    var start = 1
+    if (complete) {
+        start = 0
+    }
+    for i in start..<args.count {
+        print("Parameter \(i): \(args[i])")
+    }
+}
+
+var username = NSUserName()
+sayHello(name: username)
+username = NSFullUserName()
+sayHello(name: username)
+
+printArgs(complete: true)
